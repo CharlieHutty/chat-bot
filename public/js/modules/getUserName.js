bedc4ad6dcfab8a09ca48421cb45d590c9771dce
Charlie Hutty <charlie.hutty@thespencergroup.co.uk>
2021-10-15 15:33:44 +0100

    Last commit

--- public/js/modules/getUserName.js
+++ public/js/modules/getUserName.js
@@ -2,21 +2,25 @@ export function getUserName() {
     $(function () {
         $('form').on('submit', function (e) {
             e.preventDefault();
-
-            $.ajax({
-                type: "POST",
-                url: 'https://127.0.0.1:8000/user-name.php',
-                data: $('form').serialize(),
-                success: function (response) {
-                    let div = document.createElement('div');
-                    let tag = document.createElement("p");
-                    let text = document.createTextNode('Hello, ' + response + ' what category of Holiday would you like?');
-                    tag.appendChild(text);
-                    div.appendChild(tag)
-                    let element = document.getElementsByClassName('bot-questions')[0];
-                    element.appendChild(div);
-                }
-            });
+            let isPost = 0;
+            if (isPost === 0) {
+                $.ajax({
+                    type: "POST",
+                    url: 'https://127.0.0.1:8000/user-name.php',
+                    data: $('form').serialize(),
+                    success: function (response) {
+                        let div = document.createElement('div');
+                        let tag = document.createElement("p");
+                        console.log(response);
+                        let text = document.createTextNode(response + ' what category of Holiday would you like?');
+                        tag.appendChild(text);
+                        div.appendChild(tag)
+                        let element = document.getElementsByClassName('bot-questions')[0];
+                        element.appendChild(div);
+                        isPost = 1;
+                    }
+                });
+            }
         });
     });
 }
--- src/Controller/UserDetailsController.php
+++ src/Controller/UserDetailsController.php
@@ -9,10 +9,18 @@ class UserDetailsController extends AbstractController
 {
     public function getUserName(): Response
     {
+        date_default_timezone_set('Europe/London');
         if(isset($_POST["user-name"])) {
+            $date = date('H:i:s');
+            if ($date < '12:00:00') {
+                $message = 'Good Morning';
+            } else {
+                $message = 'Good Afternoon';
+            }
             if(isset($_POST['user-name'])) {
                 $name = $_POST['user-name'];
-                return new Response($name);
+                $message = $message .', '. $name;
+                return new Response($message);
             }
         }
         return new Response("
